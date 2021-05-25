7fd0c3a4c176cfd8c289eaa3354aea757e14dd3d
weston-bailey <64712575+weston-bailey@users.noreply.github.com>
2021-05-25 11:47:38 -0700

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -111,24 +111,17 @@ const talkingHeadsAlbums = [
 ];
 
 // 1. Create an object literal called `band`.
-let band = {};
 
 // 2. Give it the property `name` and set it to "Talking Heads"
-band.name = "Talking Heads";
 
 // 3. Give it the property `members` and set it to an array with a single
 //    string, "David Byrne", in it.
-band.members = ["David Byrne"];
 
 // 4. Give it the property `albums` and set it to the array stored in the
 //    variable talkingHeadsAlbums
-band.albums = talkingHeadsAlbums;
 
 // 5. Add "Tiny Weymouth", "Chris Franz" and "Jerry Harrison" to the members
 //    array.
-band.members.push("Tiny Weymouth");
-band.members.push("Chris Franz");
-band.members.push("Jerry Harrison");
 
 ////////////////////////////////////////////////
 // Part 3: Conditional Logic
